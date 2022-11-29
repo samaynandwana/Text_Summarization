fe515487e414f70da49e3875634a782de517e4f9
Shreyas <schicker@purdue.edu>
2022-11-29 14:18:57 -0500

    restricts uploads to txt

--- summarize.py
+++ summarize.py
@@ -1,19 +1,34 @@
-from gensim.summarization.summarizer import summarize
-
-text = "In late summer 1945, guests are gathered for the wedding reception of Don Vito Corleones " + \
-       "daughter Connie (Talia Shire) and Carlo Rizzi (Gianni Russo). Vito (Marlon Brando),"  + \
-       "the head of the Corleone Mafia family, is known to friends and associates as Godfather. "  + \
-       "He and Tom Hagen (Robert Duvall), the Corleone family lawyer, are hearing requests for favors "  + \
-       "because, according to Italian tradition, no Sicilian can refuse a request on his daughter's wedding " + \
-       " day. One of the men who asks the Don for a favor is Amerigo Bonasera, a successful mortician "  + \
-       "and acquaintance of the Don, whose daughter was brutally beaten by two young men because she"  + \
-       "refused their advances; the men received minimal punishment from the presiding judge. " + \
-       "The Don is disappointed in Bonasera, who'd avoided most contact with the Don due to Corleone's" + \
-       "nefarious business dealings. The Don's wife is godmother to Bonasera's shamed daughter, " + \
-       "a relationship the Don uses to extract new loyalty from the undertaker. The Don agrees " + \
-       "to have his men punish the young men responsible (in a non-lethal manner) in return for " + \
-        "future service if necessary."
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
           
-x = summarize(text)
-
-print(x)
+def summarize(text, per):
+    nlp = spacy.load('en_core_web_sm')
+    doc= nlp(text)
+    tokens=[token.text for token in doc]
+    word_frequencies={}
+    for word in doc:
+        if word.text.lower() not in list(STOP_WORDS):
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    max_frequency=max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word]=word_frequencies[word]/max_frequency
+    sentence_tokens= [sent for sent in doc.sents]
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():                            
+                    sentence_scores[sent]=word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent]+=word_frequencies[word.text.lower()]
+    select_length=int(len(sentence_tokens)*per)
+    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
+    final_summary=[word.text for word in summary]
+    summary=''.join(final_summary)
+    return summary
